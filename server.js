97458de6d90dbc5b687e2326dff0b3be8e71866f
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-09-13 23:02:56 -0400

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/tip/:total/:tipPercentage', (req, res) =>{
     res.send( `${tip}`);
 });
 
+// Magic 8 Ball
+app.get('/magic/:question', (req, res) =>{
+    const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];  
+    res.send(`
+        <h1>${responses[Math.floor(Math.random() * responses.length)]}</h1>
+    `)
+})
+
+
 
 app.listen(port, ()=>{
     console.log("Listening...");
