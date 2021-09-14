5cb449bd8b53ecd42d42c3f043819b804ff632f2
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-09-13 20:59:08 -0400

    Greeting express application created

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/greeting/:name', (req, res) =>{
+    res.send("What's up " + req.params.name )
+})
+
+app.listen(port, ()=>{
+    console.log("Listening...")
+})
