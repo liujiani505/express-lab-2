a2bff1549956f2246a72d24f2569db6e892fe099
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-09-13 22:21:28 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get('/greeting/:name', (req, res) =>{
-    res.send("What's up " + req.params.name )
-})
+    res.send("What's up " + req.params.name);
+});
+
+// Tip Calculator
+app.get('/tip/:total/:tipPercentage', (req, res) =>{
+    const tip = req.params.total * req.params.tipPercentage/100
+    res.send( `${tip}`);
+});
+
 
 app.listen(port, ()=>{
-    console.log("Listening...")
+    console.log("Listening...");
 })
+
